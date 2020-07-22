0fbd295c6a1a806ee9e7a5fdc3e9c15d2c087902
Yu Zhang <zhangy51@utas.edu.au>
2020-07-22 10:55:08 +1000

    Add project files.

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] fredoubled)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
