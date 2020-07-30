53f05f4ef70cc09540d0c76339283c0f10f402ff
Yu Zhang <zhangy51@utas.edu.au>
2020-07-30 10:01:25 +1000

    Practical 2: Arithmetic, Program Tracing & Decisions 3 Making some simple decisions

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -2,11 +2,36 @@
 
 namespace HelloWorld
 {
-    class HelloWorld
+    class Reading3
     {
-        static void Main(string[] fredoubled)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string s;
+            int n;
+            bool b;
+            Console.Write("Please enter a positive integer n: ");
+            do {                
+                s = Console.ReadLine();
+                b = Int32.TryParse(s, out n);
+                {
+                    if (b && n > 0)
+                    {
+                        if (n % 2 == 0)
+                        {
+                            Console.WriteLine(n + " % 2 == 0, n = " + n + " / 2");
+                            n /= 2;
+                            //                        Console.Write("n = " + n);
+                        }
+                        else
+                        {
+                            Console.WriteLine(n + " % 2 != 0, n = " + n + " * 3 + 1");
+                            n = n * 3 + 1;
+                        }
+                        Console.WriteLine("n = " + n);
+                    }
+                    else Console.Write(s + " is not a positive integer. Please enter again: ");
+                }
+            } while (b == false || n <= 0);
         }
     }
 }
